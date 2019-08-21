4a2073d58f9116a3014aabfbefa6e6f347d36720
Justin Tom <jtom@revacomm.local>
2019-08-21 05:42:28 -1000

    added deployments for straight kubernetes

--- CoreApiContainer/Startup.cs
+++ CoreApiContainer/Startup.cs
@@ -13,39 +13,39 @@ using Microsoft.Extensions.Options;
 
 namespace CoreApiContainer
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseHsts();
+			}
 
 			app.UseCors(builder => builder.AllowAnyOrigin()
 										.AllowAnyMethod()
 										.AllowAnyHeader());
 
 			app.UseHttpsRedirection();
-            app.UseMvc();
-        }
-    }
+			app.UseMvc();
+		}
+	}
 }
